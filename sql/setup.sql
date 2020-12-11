8b41f6b81453d23da40df73a0131f1bd357ac29c
dl-watson <dlarkinwatson@gmail.com>
2020-12-11 14:09:33 -0800

    saving pgAdmin query testing

--- sql/setup.sql
+++ sql/setup.sql
@@ -17,3 +17,28 @@ CREATE TABLE enrollments (
     class_id BIGINT REFERENCES classes(id),
     PRIMARY KEY (student_id, class_id)
 );
+
+
+-- pgADMIN QUERIES
+-- INSERT INTO students (name) VALUES ('Dee');
+-- INSERT INTO students (name) VALUES ('Richard'); 
+-- INSERT INTO students (name) VALUES ('Jena');
+-- SELECT * FROM students;
+
+-- INSERT INTO classes (title) VALUES ('DEV 101');
+-- INSERT INTO classes (title) VALUES ('FOUNDATIONS I'); 
+-- INSERT INTO classes (title) VALUES ('FOUNDATIONS II');
+-- SELECT * FROM classes;
+
+-- INSERT INTO enrollments (student_id, class_id) VALUES (1, 1), (1, 2)
+
+-- SELECT  
+-- 	students.name,
+-- 	array_agg(classes.title)
+-- FROM 
+-- 	enrollments
+-- JOIN students
+-- ON enrollments.student_id = students.id
+-- JOIN classes
+-- ON enrollments.class_id = classes.id
+-- GROUP BY students.name
