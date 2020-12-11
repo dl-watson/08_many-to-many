cd94f7eaa67817e753da6bcb0162dd5f9a835e39
dl-watson <dlarkinwatson@gmail.com>
2020-12-11 14:14:06 -0800

    successfully failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,19 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
-const request = require('supertest');
-const app = require('../lib/app');
+const fs = require("fs");
+const pool = require("../lib/utils/pool");
+const request = require("supertest");
+const app = require("../lib/app");
 
-describe('08_many-to-many routes', () => {
+describe("08_many-to-many routes", () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it("tests express route", async () => {
+    const res = await request(app).get("/");
+    expect(res.body).toEqual({ hello: "world" });
   });
 });
